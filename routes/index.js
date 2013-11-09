0fac16466a4d870bd4e77649a9add92e4059d5cd
adamjacobbecker <ad@mbecker.cc>
2013-11-08 23:56:21 -0800

    get the real count

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,20 @@
 var Signature = require('../signature');
 
 exports.index = function(req, res){
-  Signature.find({}).sort('created').limit(10).exec(function(err, results){
-
-    res.send({
-      signed: false,
-      signers: results
+  Signature.count(function(err, count){
+    Signature.find({}).sort('created').limit(10).exec(function(err, results){
+      res.send({
+        signed: false,
+        signers: results,
+        count: count
+      });
     });
-
   });
 };
 
 exports.more = function(req, res){
 
-  Signature.find().sort('created').skip(parseInt(req.params.skip)).limit(20).exec(function(err, results){
+  Signature.find().sort('created').skip(parseInt(req.query.skip || 0)).limit(20).exec(function(err, results){
     res.send({
       signers: results
     })
