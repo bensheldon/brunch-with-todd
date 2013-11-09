7d1ebb466cb423e256ecfd362de3255866d7df2c
adamjacobbecker <ad@mbecker.cc>
2013-11-09 01:10:46 -0800

    no need

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ exports.index = function(req, res){
   Signature.count(function(err, count){
     Signature.find({ picture_url: {'$ne': null } }).sort('-created').limit(10).exec(function(err, results){
       res.send({
-        signed: false,
         signers: results,
         count: count
       });
